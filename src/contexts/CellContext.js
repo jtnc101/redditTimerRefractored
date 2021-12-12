0832de872bfb5c00b8b266cceaea2a90d3062eb9
jtnc101 <71797572+jtnc101@users.noreply.github.com>
2021-12-12 15:35:45 -0500

    updated CellContext

--- src/contexts/CellContext.js
+++ src/contexts/CellContext.js
@@ -6,7 +6,8 @@ export const CellProvider = (props) => {
 
     // Cell holds information pertaining to the cell clicked on the heat map. Its value determines how many posts need to be 
     // generated in the Posts section. 
-    // cell is an array with 3 values. cell[0] is row of the heat map. cell[1] is column of the heat map. cell[2] is the value in the cell. 
+    // cell is an array with 3 values. cell[0] is row of the heat map. cell[1] is column of the heat map.
+    // cell[2] is the value in the cell. 
     // ie. the number of posts at that specific timeslot.
 
     const [cell, setCell] = useState([]);
